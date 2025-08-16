eba14da34323c58d209a20f8811f56a36d98bb10
ndhiep03it <nguyendieuhiep212@gmail.com>
2025-08-16 22:04:10 +0700

    update

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,39 +1,16 @@
 import express from 'express';
-import userController from '../controllers/userController.js';
-import bookController from '../controllers/bookController.js';
-import categoryController from '../controllers/categoryController.js'
-import authController from '../controllers/authController.js';
+import upload from '../config/multer.js'; // Assuming multer is configured in this file
 
-const upload = require('../config/multer.js'); // Assuming multer is configured in this file
 const router = express.Router();
 
-
 const webRoutes = (app) => {
-    // Define your routes here
-    router.get('/', authController.show);
-    router.post('/', authController.login);
+  // Route homepage
+  router.get('/', (req, res) => {
+    res.send('Welcome to the homepage!');
+  });
 
-    // User routes
-    router.get('/manage-user', userController.index);
-    router.post('/create-user', userController.create);
-    router.get('/delete-user/:id', userController.destroy);
-    router.get('/edit-user/:id', userController.edit);
-    router.post('/update-user/:id', userController.update);
-    //Book routes
-    router.get('/manage-book', bookController.index);
-    router.post('/create-book', upload.single('cover'), bookController.create);
-    router.get('/create-book', upload.single('cover'), bookController.getCreate);
-    router.get('/edit-book/:id', upload.single('cover'), bookController.edit);
-    router.post('/update-book/:id', upload.single('cover'), bookController.update);
-    router.get('/delete-book/:id', bookController.destroy);
-    // Category routes
-    router.get('/manage-category', categoryController.index);
-    router.post('/create-category', categoryController.create);
-    router.get('/edit-category/:id', categoryController.edit);
-    router.post('/update-category/:id', categoryController.update);
-    router.get('/delete-category/:id', categoryController.destroy);
-
-    app.use('/', router);
-}
+  // Mount router vào app
+  app.use('/', router);
+};
 
 export default webRoutes;
